78e5e905f9f89e9f41c367f8d49eb3d3665734a1
Lucas Smaniotto <lucas91755@gmail.com>
2022-09-08 10:41:52 -0300

    Truthy e Falsy

---
+++ Tipos, variáveis e funções/Truthy e falsy/truthy-falsy.js
@@ -0,0 +1,30 @@
+// boolean
+
+const userLogged = true;
+const billPaid = false;
+
+// truthy or falsy
+//  0 -> false
+//  1 -> true
+//  == -> resultado
+//  === -> igualdade
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// null -> vazio | undifined -> não definido
+
+let variable;
+let varNull =  null;
+
+console.log(variable)
+console.log(varNull)
+
+let number = 3;
+let name = "Lucas";
+
+console.log(typeof number)
+console.log(typeof name)
+console.log(typeof variable)
+console.log(typeof varNull)
