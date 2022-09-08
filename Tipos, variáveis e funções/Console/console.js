80f1c57ce8970840380f27762c85e6692018732f
Lucas Smaniotto <lucas91755@gmail.com>
2022-09-08 12:10:41 -0300

    Console API

---
+++ Tipos, variáveis e funções/Console/console.js
@@ -0,0 +1,21 @@
+// log -> registro
+
+const variable = true;
+
+console.log(42)
+console.log("Hello there")
+console.log(variable)
+
+// tratamento de erro
+
+console.error("Oops!")
+console.warn("Watch out...")
+//console.clear()
+
+/* Outros métodos:
+
+    console.table() -> para visualizar de forma mais organizada informações tabulares;
+    console.time() & console.timeEnd() -> para temporizar período que uma operação de código leva para ser iniciada e concluída;
+    console.trace() -> para exibir a stacktrace de todos  arquivos chamados por onde o código executado passou durante a execução.
+
+*/
