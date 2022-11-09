e0ab604ecb9ed4858284719323269b481a677870
Lucas Smaniotto <lucas91755@gmail.com>
2022-11-09 09:49:52 -0300

    Iteração de Listas com Textos Dinâmicos e Função anônima

--- Javascript - Frontend/Alura Midi/main.js
+++ Javascript - Frontend/Alura Midi/main.js
@@ -1,5 +1,14 @@
-function playSoundPom(){
-    document.querySelector('#som_tecla_pom').play();
+function playSound(idAudioElement) {
+    document.querySelector(idAudioElement).play();
 }
 
 const keysList = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < keysList.length; i++) {
+    const key = keysList[i];
+    const note = key.classList[1];
+    const idAudio = `#som_${note}`;
+    key.onclick = function () {
+        playSound(idAudio);
+    }
+}
