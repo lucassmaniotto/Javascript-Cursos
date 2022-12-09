888ce2d1d6f57d5f8014f9d43d4d3c633744b173
Lucas Smaniotto <lucas91755@gmail.com>
2022-12-09 14:09:30 -0300

    Função de sorteio

---
+++ Javascript - Frontend/Reconhecimento de Voz/app/drawNumber.js
@@ -0,0 +1,15 @@
+const minValue = 1,
+      maxValue = 1000;
+
+const minValuElement = document.querySelector('#min-value');
+minValuElement.innerHTML = minValue;
+
+const maxValueElement = document.querySelector('#max-value');
+maxValueElement.innerHTML = maxValue;
+
+const secretNumber = drawNumber();
+
+function drawNumber() {
+    return parseInt(Math.random() * maxValue + 1);
+}
+
