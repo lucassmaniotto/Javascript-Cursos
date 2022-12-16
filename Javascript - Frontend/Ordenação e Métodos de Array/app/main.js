6d384d0d7b9bd4929c722bcdaa48e9e00823aff6
Lucas Smaniotto <lucas91755@gmail.com>
2022-12-16 12:02:11 -0300

    Métodos de array com DOM

---
+++ Javascript - Frontend/Ordenação e Métodos de Array/app/main.js
@@ -0,0 +1,32 @@
+let books = [];
+const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+const elementForInsertBooks = document.querySelector('#livros');
+
+getSearchBooksAPI();
+
+async function getSearchBooksAPI() {
+  const response = await fetch(endpointAPI);
+  books = await response.json();
+  console.table(books);
+  renderBooks();
+}
+
+function renderBooks() {
+  elementForInsertBooks.innerHTML = '';
+  books.forEach(book => {
+    elementForInsertBooks.innerHTML += `
+      <div class="livro">
+          <img class="livro__imagens" src="${book.imagem}" alt="${book.alt}" />
+          <h2 class="livro__titulo">
+            ${book.titulo}
+          </h2>
+          <p class="livro__descricao">${book.autor}</p>
+          <p class="livro__preco" id="preco">R$${book.preco}0</p>
+          <div class="tags">
+            <span class="tag">${book.categoria}</span>
+          </div>
+      </div>
+    `
+  });
+}
